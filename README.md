da7c464239b6e2f072d65c9ba810fddd7fdc629e
tsujimitsu <tsujimitsu@gmail.com>
2016-10-02 00:02:06 +0900

    add new dashboard codes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# clash_of_clan_dashboard
-Dashboard for Clash of Clan API
+Clash of Clan Dashboard
+====
+* Dashboard for Clash of Clan API
+
+usage
+----
+
+    $ git clone https://github.com/tsujimitsu/clash_of_clan_dashboard.git
+    $ pip install -r requirements.txt
+    $ vi app.py
+    -> edit AUTH_TOKEN(change to your token)
+    $ python app.py
+    $ curl localhost:10080
+    $ curl localhost:10080/clans/<clan_tag>
+    -> clan_tag must do URL encoding
---
+++ app.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+import flask
+import requests
+import urllib
+
+AUTH_TOKEN = "<YOUR TOKEN>"
+BASE_ENDPOINT = "https://api.clashofclans.com/v1"
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/clans/<clan_tag>', methods=['GET'])
+def get_clan(clan_tag):
+    clan_tag_encode = urllib.quote_plus(clan_tag)
+    header = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + AUTH_TOKEN
+    }
+    messages = requests.get(BASE_ENDPOINT + "/clans/" + clan_tag_encode, headers=header)
+    return flask.render_template('get_clan.html', msgs=messages.json())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10080, debug=True)
